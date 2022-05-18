dc9481a77e8e9b2ba5342b213b051a1568d94921
Eduard <eduard.scobioala@mail.com>
2022-05-18 09:39:55 +0300

    added the logic for login process

---
+++ App/src/logic/Login.java
@@ -0,0 +1,46 @@
+package logic;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class Login {
+
+	private String username;
+	private String password;
+	
+	public Login(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	static String readFile(String path) throws IOException {
+		byte[] data = Files.readAllBytes(Paths.get(path));
+		return new String(data);
+	}
+	
+	public boolean tryLogin() {
+		
+		String jsonFileContent = null;
+		
+		try {
+			jsonFileContent = readFile("file.json");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	    
+	    java.lang.reflect.Type componentsListType = new TypeToken<ArrayList<LoginCredentials>>(){}.getType();
+	    List<LoginCredentials> _credentials = new Gson().fromJson(jsonFileContent, componentsListType);
+	    
+	    return !_credentials
+		.stream()
+		.filter(login -> login.getUsername().equals(username) && login.getPassword().equals(password))
+		.findAny()
+		.isEmpty();
+	}
+} 
